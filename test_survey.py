7c75a295970e9d39da97c548c1ccd803485fc06b
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-12 21:08:19 -0300

    refatora os testes

--- test_survey.py
+++ test_survey.py
@@ -5,20 +5,20 @@ class TestAnonymousSurvey(unittest.TestCase):
     """
     Teste AnonymousSurvey
     """
-    def test_store_single_response(self):
+    def setUp(self):
         question = 'What langauge did you first learn to speak? '
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Portuguese', 'Spanish']
+        
+    def test_store_single_response(self):
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
     
     def test_store_tree_responses(self):
-        question = 'What langauge did you first learn to speak? '
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Portuguese', 'Spanish']
-        for response in responses:
-            my_survey.store_response(response)
-        for response in responses:
-            self.assertIn(response, responses)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.responses)
         
         
 unittest.main()
